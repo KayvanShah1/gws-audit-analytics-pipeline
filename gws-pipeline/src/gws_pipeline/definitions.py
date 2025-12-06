f80f1f1d53bcbf5c1ec402f4843e1b32d0a538db
Kayvan Shah <59110083+KayvanShah1@users.noreply.github.com>
2025-12-05 22:09:16 -0800

    add incremental data loader assets and duckdb resource

--- gws-pipeline/src/gws_pipeline/definitions.py
+++ gws-pipeline/src/gws_pipeline/definitions.py
@@ -1,15 +1,15 @@
 from dagster import Definitions
-from gws_pipeline.defs.ingestion.assets import incremental_assets
+from gws_pipeline.defs.ingestion.assets import incremental_fetcher_assets, incremental_loader_assets
 from gws_pipeline.defs.ingestion.resources import GoogleReportsAPIResource, StateFileResource, duckdb_motherduck
 from gws_pipeline.defs.processing.assets import processing_assets
 from gws_pipeline.jobs import job_all, schedule_hourly
 
 defs = Definitions(
-    assets=[*incremental_assets, *processing_assets],
+    assets=[*incremental_fetcher_assets, *processing_assets, *incremental_loader_assets],
     resources={
         "google_reports_api": GoogleReportsAPIResource(),
         "state_file": StateFileResource(),
-        "duckdb_md": duckdb_motherduck,
+        "duckdb_motherduck": duckdb_motherduck,
     },
     jobs=[job_all],
     schedules=[schedule_hourly],
